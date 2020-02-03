a573f6e397b29ea2a9a3d457ae3a1fe3093448eb
NaczelnyRachmistrz <michkukula@gmail.com>
2020-02-03 10:50:25 +0100

    Added generator & different input methods & working on cycle gan

---
+++ generate_samples.py
@@ -0,0 +1,61 @@
+import glob
+import os
+import numpy as np
+import imageio
+from skimage import img_as_ubyte
+from read_input import gt_dir, test_fps, read_input, read_gt, augment_photo
+import matplotlib.pyplot as plt
+
+
+# generate samples the dataset
+# channels - number of channels to pack short exposure photos into
+# If half_size set to True, resulting ground truth photo resolution will be X x Y, not 2X x 2Y (comparing to input)
+def generate_samples(n=200, samples_dir='/sata_disk/VRNN/Learning-to-See-in-the-Dark/samples/',
+                     channels=4, half_size=True):
+    input_dir = os.path.join(samples_dir, 'short')
+    sample_gt_dir = os.path.join(samples_dir, 'long')
+    if not os.path.isdir(input_dir):
+        os.makedirs(input_dir)
+        os.makedirs(os.path.join(input_dir, '100'))
+        os.makedirs(os.path.join(input_dir, '250'))
+        os.makedirs(os.path.join(input_dir, '300'))
+
+    if not os.path.isdir(sample_gt_dir):
+        os.makedirs(sample_gt_dir)
+        os.makedirs(os.path.join(sample_gt_dir, '100'))
+        os.makedirs(os.path.join(sample_gt_dir, '250'))
+        os.makedirs(os.path.join(sample_gt_dir, '300'))
+
+    for i in range(n):
+        print("Generating sample nr {}".format(i))
+        sample_fp = np.random.choice(test_fps)
+        id = os.path.basename(sample_fp)[0:5]
+        gt_files = glob.glob(gt_dir + '{}_00*.ARW'.format(id))
+        gt_fp = gt_files[0]
+        sample_exp = float(os.path.basename(sample_fp)[9:-5])
+        gt_exp = float(os.path.basename(gt_fp)[9:-5])
+        ratio = min(gt_exp / sample_exp, 300)
+        sample_photo = read_input(sample_fp, ratio, channels)
+        gt_photo = read_gt(gt_fp, half_size)
+        sample_photo, gt_photo = augment_photo(sample_photo, gt_photo)
+        sample_photo = sample_photo[0, :, :, :]
+        gt_photo = gt_photo[0, :, :, :]
+        curr_input_dir = os.path.join(input_dir, str(int(ratio)))
+        curr_gt_dir = os.path.join(sample_gt_dir, str(int(ratio)))
+        output_sample_path = os.path.join(curr_input_dir, os.path.basename(sample_fp))[:-4] + '_{}.png'.format(i)
+        output_gt_path = os.path.join(curr_gt_dir, os.path.basename(gt_fp))[:-4] + '_{}.png'.format(i)
+        imageio.imsave(output_sample_path, img_as_ubyte(sample_photo))
+        imageio.imsave(output_gt_path, img_as_ubyte(gt_photo))
+
+
+# Auxiliary method - shows a plot comparing given image to its ground truth
+def show_img(input, gt):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax1.imshow(input[0, :, :, :])
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.imshow(gt[0, :, :, :])
+    plt.show()
+
+
+generate_samples(30, '/sata_disk/VRNN/Learning-to-See-in-the-Dark/samples6/', 3)
--- read_input.py
+++ read_input.py
@@ -1,26 +1,24 @@
 # Responsible for handling input dataset & data augmentation
-
 import glob
 import os
 import numpy as np
 import rawpy
-import matplotlib.pyplot as plt
 
 # Short (input) and long (ground truth) exposure time photos directories
 input_dir = '/sata_disk/VRNN/Learning-to-See-in-the-Dark/dataset/Sony/short/'
 gt_dir = '/sata_disk/VRNN/Learning-to-See-in-the-Dark/dataset/Sony/long/'
 
-train_fps = glob.glob(gt_dir + '0*.ARW') # full paths
+train_fps = glob.glob(gt_dir + '*.ARW') # full paths
 train_ids = [int(os.path.basename(train_fp)[0:5]) for train_fp in train_fps]
 
-test_fps = glob.glob(input_dir + '1*.ARW') # full paths
+test_fps = glob.glob(input_dir + '*.ARW') # full paths
 test_ids = [int(os.path.basename(test_fp)[0:5]) for test_fp in test_fps]
 
 patch_size = 512 # cropped image size
 
 
 # pack Bayer image to 4 channels
-def pack_raw(raw):
+def pack_raw_into4(raw):
     im = raw.raw_image_visible.astype(np.float32)
     im = np.maximum(im - 512, 0) / (16383 - 512)
 
@@ -36,17 +34,28 @@ def pack_raw(raw):
     return out
 
 
+def pack_raw_into3(raw):
+    im = raw.postprocess(use_camera_wb=True, half_size=True, no_auto_bright=True, output_bps=16)
+    im = im.astype(np.float32)
+    im = np.maximum(im - 512, 0) / (16383 - 512)
+    return im
+
+
 # Reads input photo using a given path
-def read_input(filepath, ratio=100):
+def read_input(filepath, ratio=100, channels=4):
     raw_photo = rawpy.imread(filepath)
-    image = np.expand_dims(pack_raw(raw_photo), axis=0) * ratio
+    if channels == 4:
+        image = np.expand_dims(pack_raw_into4(raw_photo), axis=0) * ratio
+    else:
+        image = np.expand_dims(pack_raw_into3(raw_photo), axis=0) * ratio
     return image
 
 
-# Reads ground truth photo using agiven path
-def read_gt(filepath):
+# Reads ground truth photo using a given
+# If half_size set to True, resulting photo resolution will be X x Y, not 2X x 2Y
+def read_gt(filepath, half_size=False):
     gt_raw = rawpy.imread(filepath)
-    im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+    im = gt_raw.postprocess(use_camera_wb=True, half_size=half_size, no_auto_bright=True, output_bps=16)
     return np.expand_dims(np.float32(im / 65535.0), axis=0)
 
 
@@ -61,7 +70,7 @@ def augment_photo(input, gt):
     xx = np.random.randint(0, W - patch_size)
     yy = np.random.randint(0, H - patch_size)
     input_patch = input[:, yy:yy + patch_size, xx:xx + patch_size, :]
-    gt_patch = gt[:, yy * 2:yy * 2 + patch_size * 2, xx * 2:xx * 2 + patch_size * 2, :]
+    gt_patch = gt[:, yy:yy + patch_size , xx:xx+patch_size, :]
 
     if np.random.randint(2, size=1)[0] == 1:  # random flip
         input_patch = np.flip(input_patch, axis=1)
@@ -75,20 +84,3 @@ def augment_photo(input, gt):
 
     input_patch = np.minimum(input_patch, 1.0)
     return input_patch, gt_patch
-
-
-# Auxiliary method - shows a plot comparing given image to its ground truth
-def show_img(input, gt):
-    fig = plt.figure()
-    ax1 = fig.add_subplot(2, 2, 1)
-    ax1.imshow(input[0, :, :, :])
-    ax2 = fig.add_subplot(2, 2, 2)
-    ax2.imshow(gt[0, :, :, :])
-    plt.show()
-
-
-# example
-input_photo = read_input(os.path.join(input_dir, '00002_00_0.1s.ARW'))
-gt_photo = read_gt(os.path.join(gt_dir, '00002_00_10s.ARW'))
-input_photo, gt_photo = augment_photo(input_photo, gt_photo)
-show_img(input_photo, gt_photo)
